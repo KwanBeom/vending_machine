791c36642744584b40fab8408caca0c5da5ea743
beomkwan.choi <3031011@naver.com>
2023-05-02 11:37:16 +0900

    밴딩머신 final

--- js/classes/vendingMachineEvents.js
+++ js/classes/vendingMachineEvents.js
@@ -8,23 +8,25 @@ class VendingMachineEvents {
         this.btnPut = vMachine.querySelector("button[form='deposit']");
         this.btnGet = vMachine.querySelector(".get-btn");
         this.stagedList = vMachine.querySelector(".scroll-box");
-        
 
         const myInfo = document.querySelector(".right-content");
         this.myMoney = myInfo.querySelector(".money-box p span");
         this.getList = myInfo.querySelector(".scroll-box");
-        this.txtTotal = myInfo.querySelector(".total-money");
+        this.totalCost = myInfo.querySelector(".total-money");
     }
     stagedItemGenerator(target) {
-        const stagedItem = document.createElement('li')
-        stagedItem.classList.add("scroll-item")
+        const stagedItem = document.createElement("li");
+        stagedItem.classList.add("scroll-item");
+        stagedItem.dataset.item = target.dataset.item;
+        stagedItem.dataset.cost = target.dataset.cost;
+
         stagedItem.innerHTML = `
             <img src="../../images/${target.dataset.img}" alt=""> 
                 ${target.dataset.item}
-            <strong class="count"></strong>
-        `
+            <strong class="count">1</strong>
+        `;
 
-        this.stagedList.append(stagedItem)
+        this.stagedList.append(stagedItem);
     }
 
     bindEvent() {
@@ -51,11 +53,10 @@ class VendingMachineEvents {
                 if (inpCost <= myMoneyVal && inpCost > 0) {
                     // 입금액 <= 소지금
                     this.myMoney.textContent =
-                        new Intl.NumberFormat().format(myMoneyVal - inpCost) +
-                        "원";
+                        new Intl.NumberFormat().format(myMoneyVal - inpCost) + "원";
                     this.balance.textContent =
-                        new Intl.NumberFormat().format(balanceVal + inpCost) +
-                        "원";
+                        new Intl.NumberFormat().format(balanceVal + inpCost) + "원";
+                    this.inputCostEl.value = ''                            
                 } else {
                     // 입금액 > 소지금
                     alert("잔액이 부족합니다.");
@@ -67,21 +68,15 @@ class VendingMachineEvents {
          * 1) 반환버튼 누르면 소지금 === 잔액 + 소지금
          * 2) 반환버튼 누르면 잔액창 초기화
          */
-        
+
         this.btnReturn.onclick = () => {
             // 소지금
-            const myMoneyVal = parseInt(
-                this.myMoney.textContent.replaceAll(",", "")
-            );
+            const myMoneyVal = parseInt(this.myMoney.textContent.replaceAll(",", ""));
             // 잔액
-            const balanceVal = parseInt(
-                this.balance.textContent.replaceAll(",", "")
-            );
+            const balanceVal = parseInt(this.balance.textContent.replaceAll(",", ""));
 
             if (balanceVal) {
-                this.myMoney.textContent =
-                    new Intl.NumberFormat().format(balanceVal + myMoneyVal) +
-                    "원";
+                this.myMoney.textContent = new Intl.NumberFormat().format(balanceVal + myMoneyVal) + "원";
                 this.balance.textContent = 0 + "원";
             }
         };
@@ -98,22 +93,70 @@ class VendingMachineEvents {
                 const balanceVal = parseInt(
                     this.balance.textContent.replaceAll(",", "")
                 );
-                const targetCost = parseInt(event.currentTarget.dataset.cost)
+                const targetEl = event.currentTarget
+                const targetCost = parseInt(targetEl.dataset.cost);
+                const stagedListItem = this.stagedList.querySelectorAll("li")
+                let isStaged = false;
 
-                if(balanceVal >= targetCost){
-                    this.balance.textContent = new Intl.NumberFormat().format(balanceVal - targetCost) + '원'
+                if (balanceVal >= targetCost) {
+                    this.balance.textContent =
+                        new Intl.NumberFormat().format(balanceVal - targetCost) + "원";
 
-                    this.stagedItemGenerator(event.currentTarget)
-                    for(const item of this.stagedList){
-                        
+                    for (const item of stagedListItem) {
+                        if(targetEl.dataset.item === item.dataset.item){
+                            item.querySelector("strong").firstChild.textContent = parseInt(item.querySelector("strong").firstChild.textContent) + 1
+
+                            isStaged = true;
+                            break;
+                        }
+                    }
+                    if(!isStaged) {
+                        this.stagedItemGenerator(event.currentTarget)
+                    }
+
+                    // 자판기 콜라 갯수 차감
+                    targetEl.dataset.count--;
+                    if(parseInt(targetEl.dataset.count) === 0){
+                        targetEl.classList.add("soldout")
+                        targetEl.disabled = true;
                     }
                 } else {
-                    alert("잔액이 부족합니다")
+                    alert("잔액이 부족합니다");
                 }
-                console.log(targetCost)
             };
         });
+                /**
+     * 4. 획득 버튼 기능
+     * 1) 장바구니에 있는 음료수 목록 => 획득한 음료 목록 이동
+     * 2) 획득한 음료 금액 합산해 총금액에 input
+     */
+    this.btnGet.onclick = () => {
+        const itemStagedList = this.stagedList.querySelectorAll("li")
+        const itemGetList = this.getList.querySelectorAll("li")
+        let totalPrice = 0
+
+        for(const itemStaged of itemStagedList){
+            let isGet = false;
+            for(const itemGet of itemGetList){
+                // 장바구니의 item과 획득 목록의 item과 비교
+                if(itemStaged.dataset.item === itemGet.dataset.item){
+                    itemGet.querySelector("strong").firstChild.textContent = 
+                        parseInt(itemGet.querySelector("strong").firstChild.textContent) + parseInt(itemStaged.querySelector("strong").firstChild.textContent);
+                    isGet = true
+                    break;
+                }
+            }
+            if(!isGet){
+                this.getList.append(itemStaged);
+            }
+        }
+        this.stagedList.innerHTML = null
+
+        this.getList.querySelectorAll("li").forEach(item => {
+            totalPrice += parseInt(item.dataset.cost) * parseInt(item.querySelector(".count").firstChild.textContent)
+        })
+        this.totalCost.textContent = new Intl.NumberFormat().format(totalPrice)
+    }
     }
 }
-
 export default VendingMachineEvents;
